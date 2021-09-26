43c54c9f3a8b6d631a59bbe3acc78d0a4abeffb4
AlessandroPerazzetta <alessandro.perazzetta@gmail.com>
2021-09-26 21:34:58 +0200

    Update install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+CURRENT_USER=$(whoami)
+
 # Colors definition
 BLACK='\033[0;30m'
 DGRAY='\033[1;30m'
@@ -18,110 +20,199 @@ LGRAY='\033[0;37m'
 WHITE='\033[1;37m'
 NC='\033[0m' # No Color
 
-CURRENT_USER=$(whoami)
-printf "${YELLOW}Current user: ${LGRAY}$CURRENT_USER\n${NC}"
-  
-printf "${YELLOW}Updating system...\n${NC}"
-sudo apt update
-sudo apt upgrade
+cmd=(dialog --title "Automated packages installation" --backtitle "Mint Post Install" --no-collapse --separate-output --checklist "Select options:" 22 76 16)
+options=(
+0 "Personal resources" on
+1 "bwm-ng" on
+2 "screen" on
+3 "neovim" on
+4 "filezilla" on
+5 "meld" on
+6 "vlc" on
+7 "git" on
+8 "htop" on
+9 "brave-browser" on
+10 "remmina" on
+11 "vscodium" on
+12 "dbeaver" on
+13 "smartgit" on
+14 "keepassxc" on
+15 "qownnotes" on
+16 "virtualbox" on
+17 "kicad" on
+18 "telegram" on
+19 "rust" on
+20 "python 3.6.15 (src install)" off)
 
-printf "${YELLOW}Install some packages...\n${NC}"
-sudo apt -y install bwm-ng screen neovim filezilla meld vlc git htop jq python3-serial build-essential
-sudo apt -y install apt-transport-https curl
+choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
+clear
 
-printf "${YELLOW}Set default editor...\n${NC}"
-update-alternatives --config editor
+if [ ${#choices} -gt 0 ]
+then
+    printf "${YELLOW}Updating system...\n${NC}"
+    sleep 1
+    sudo apt update
+    sudo apt upgrade
 
-printf "${YELLOW}Remove others editor...\n${NC}"
-sudo apt -y remove nano ed
+    printf "${YELLOW}Install required packages...\n${NC}"
+    sleep 1
+    sudo apt -y install build-essential apt-transport-https curl python3-serial
 
-printf "${YELLOW}Set and install python3 as default python...\n${NC}"
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+    printf "${YELLOW}Add user to dialout group (ref: /dev/ttyUSBx Error opening serial port)...\n${NC}"
+    sudo usermod -a -G dialout $CURRENT_USER
 
-printf "${YELLOW}Installing Brave...\n${NC}"
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser.list
-sudo apt update    
-sudo apt -y install brave-browser    
+    for choice in $choices
+    do
+        case $choice in
+            0)
+                printf "${YELLOW}Installing PERSONAL RESOURCES...\n${NC}"
+                printf "${YELLOW}Installing aliase resources...\n${NC}"
+                printf "alias l='ls -lah'\nalias cls='clear'" >> ~/.bash_aliases
+                printf "${YELLOW}Installing Xed resources...\n${NC}"
+                mkdir -p ~/.local/share/xed/styles/
+                curl -fsSLo ~/.local/share/xed/styles/kat-ng.xml https://raw.githubusercontent.com/AlessandroPerazzetta/xed-themes/main/kat-ng.xml
+                ;;
+            1)
+                printf "${YELLOW}Installing bwm-ng...\n${NC}"
+                sudo apt -y install bwm-ng
+                ;;
+            2)
+                printf "${YELLOW}Installing screen...\n${NC}"
+                sudo apt -y install screen
+                ;;
+            3)
+                printf "${YELLOW}Installing neovim...\n${NC}"
+                sudo apt -y install neovim
 
-printf "${YELLOW}Installing Remmina...\n${NC}"
-sudo apt-add-repository ppa:remmina-ppa-team/remmina-next
-sudo apt update
-sudo apt -y install remmina remmina-plugin-rdp remmina-plugin-secret
+                printf "${YELLOW}Installing neovim resources...\n${NC}"
+                mkdir -p ~/.config/nvim/
+                curl -fsSLo ~/.config/nvim/init.vim https://raw.githubusercontent.com/AlessandroPerazzetta/neovim-res/main/.config/nvim/init.vim
 
-printf "${YELLOW}Installing VSCodium...\n${NC}"
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/vscodium.gpg
-echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | sudo tee /etc/apt/sources.list.d/vscodium.list
-sudo apt update && sudo apt -y install codium
+                printf "${YELLOW}Set nvim as default editor...\n${NC}"
+                update-alternatives --set editor /usr/bin/nvim
 
-printf "${YELLOW}Installing VSCodium Extension Gallery updater...\n${NC}"
-sudo curl -fsSLo /usr/local/sbin/vscodium-json-updater.sh https://raw.githubusercontent.com/AlessandroPerazzetta/vscodium-json-updater/main/update.sh
-sudo chmod +x /usr/local/sbin/vscodium-json-updater.sh
-sudo /usr/local/sbin/vscodium-json-updater.sh
+                printf "${YELLOW}Remove others editor...\n${NC}"
+                sudo apt -y remove nano ed
+                ;;
+            4)
+                printf "${YELLOW}Installing filezilla...\n${NC}"
+                sudo apt -y install filezilla
+                ;;
+            5)
+                printf "${YELLOW}Installing meld...\n${NC}"
+                sudo apt -y install meld
+                ;;
+            6)
+                printf "${YELLOW}Installing vlc...\n${NC}"
+                sudo apt -y install vlc
+                
+                printf "${YELLOW}Installing vlc media library...\n${NC}"
+                mkdir -p ~/.local/share/vlc/
+                curl -fsSLo ~/.local/share/vlc/ml.xspf https://raw.githubusercontent.com/AlessandroPerazzetta/vlc-media-library/main/ml.xspf
+                ;;
+            7)
+                printf "${YELLOW}Installing git...\n${NC}"
+                sudo apt -y install git
+                ;;
+            8)
+                printf "${YELLOW}Installing htop...\n${NC}"
+                sudo apt -y install htop
+                ;;
+            9)
+                printf "${YELLOW}Installing brave-browser...\n${NC}"
+                sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+                echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser.list
+                sudo apt update
+                sudo apt -y install brave-browser
+                ;;
+            10)
+                printf "${YELLOW}Installing remmina...\n${NC}"
+                sudo apt-add-repository -y ppa:remmina-ppa-team/remmina-next
+                sudo apt update
+                sudo apt -y install remmina remmina-plugin-rdp remmina-plugin-secret
+                ;;
+            11)
+                printf "${YELLOW}Installing vscodium...\n${NC}"
+                wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/etc/apt/trusted.gpg.d/vscodium.gpg
+                echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | sudo tee /etc/apt/sources.list.d/vscodium.list
+                sudo apt update && sudo apt -y install codium jq
 
-printf "${YELLOW}Add user to dialout...\n${NC}"
-sudo usermod -a -G dialout $CURRENT_USER
-    
-printf "${YELLOW}Installing Nemo action for VSCodium...\n${NC}"
-sudo wget https://raw.githubusercontent.com/AlessandroPerazzetta/nemo-actions-vscodium-launcher/main/codium.nemo_action -O ~/.local/share/nemo/actions/codium.nemo_action
+                printf "${YELLOW}Installing vscodium extension gallery updater...\n${NC}"
+                sudo curl -fsSLo /usr/local/sbin/vscodium-json-updater.sh https://raw.githubusercontent.com/AlessandroPerazzetta/vscodium-json-updater/main/update.sh
+                sudo chmod +x /usr/local/sbin/vscodium-json-updater.sh
+                sudo /usr/local/sbin/vscodium-json-updater.sh
 
-printf "${YELLOW}Installing DBeaver...\n${NC}"
-sudo curl -fsSLo /tmp/dbeaver-ce_latest_amd64.deb https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
-sudo dpkg -i /tmp/dbeaver-ce_latest_amd64.deb
+                printf "${YELLOW}Installing nemo action for vscodium...\n${NC}"
+                sudo wget https://raw.githubusercontent.com/AlessandroPerazzetta/nemo-actions-vscodium-launcher/main/codium.nemo_action -O ~/.local/share/nemo/actions/codium.nemo_action
+                ;;
+            12)
+                printf "${YELLOW}Installing dbeaver...\n${NC}"
+                sudo curl -fsSLo /tmp/dbeaver-ce_latest_amd64.deb https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
+                sudo dpkg -i /tmp/dbeaver-ce_latest_amd64.deb
+                ;;
+            13)
+                printf "${YELLOW}Installing smartgit...\n${NC}"
+                sudo curl -fsSLo /tmp/smartgit-21_1_1.deb https://www.syntevo.com/downloads/smartgit/smartgit-21_1_1.deb
+                sudo dpkg -i /tmp/smartgit-21_1_1.deb
+                ;;
+            14)
+                printf "${YELLOW}Installing keepassxc...\n${NC}"
+                sudo apt-add-repository -y ppa:phoerious/keepassxc
+                sudo apt update
+                sudo apt -y install keepassxc
+                ;;
+            15)
+                printf "${YELLOW}Installing qownnotes...\n${NC}"
+                sudo apt-add-repository -y ppa:pbek/qownnotes
+                sudo apt update
+                sudo apt -y install qownnotes
+                ;;
+            16)
+                printf "${YELLOW}Installing virtualbox...\n${NC}"
+                sudo wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+                echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian focal contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+                sudo apt update
+                sudo apt -y install virtualbox
 
-printf "${YELLOW}Installing SmartGit...\n${NC}"
-sudo curl -fsSLo /tmp/smartgit-21_1_1.deb https://www.syntevo.com/downloads/smartgit/smartgit-21_1_1.deb
-sudo dpkg -i /tmp/smartgit-21_1_1.deb
-
-printf "${YELLOW}Installing KeePassXC...\n${NC}"
-sudo add-apt-repository ppa:phoerious/keepassxc
-sudo apt update
-sudo apt -y install keepassxc
-
-printf "${YELLOW}Installing QOwnNotes...\n${NC}"
-sudo add-apt-repository ppa:pbek/qownnotes
-sudo apt update
-sudo apt -y install qownnotes
-
-printf "${YELLOW}Installing VirtualBox...\n${NC}"
-sudo wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian focal contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
-sudo apt update
-sudo apt -y install virtualbox
-
-printf "${LCYAN}--------------------------------------------------------------------------------\n${LRED}"
-printf "Please download VirtualBox Oracle VM VirtualBox Extension Pack from here: \n"
-printf "https://www.virtualbox.org/wiki/Downloads\n"
-printf "and install manually from VirtualBox under File > Preferences > Extensions\n"
-printf "${LCYAN}--------------------------------------------------------------------------------\n${GREEN}"
-read -n 1 -s -r -p "Press any key to continue"
-printf "\n${NC}"
-
-printf "${YELLOW}Installing Telegram...\n${NC}"
-curl -fsSLo /tmp/Telegram.xz https://telegram.org/dl/desktop/linux
-sudo tar -xf Telegram.xz -C /opt/
-
-printf "${YELLOW}Installing KiCad...\n${NC}"
-sudo add-apt-repository --yes ppa:kicad/kicad-5.1-releases
-sudo apt update
-sudo apt -y install --install-recommends kicad
-
-printf "${YELLOW}Installing Rust...\n${NC}"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-printf "${YELLOW}Installing Neovim resources...\n${NC}"
-mkdir -p ~/.config/nvim/
-curl -fsSLo ~/.config/nvim/init.vim https://raw.githubusercontent.com/AlessandroPerazzetta/neovim-res/main/.config/nvim/init.vim
-
-printf "${YELLOW}Installing Xed resources...\n${NC}"
-mkdir -p ~/.local/share/xed/styles/
-curl -fsSLo ~/.local/share/xed/styles/kat-ng.xml https://raw.githubusercontent.com/AlessandroPerazzetta/xed-themes/main/kat-ng.xml
-
-printf "${YELLOW}Installing VLC Media Library...\n${NC}"
-mkdir -p ~/.local/share/vlc/
-curl -fsSLo ~/.local/share/vlc/ml.xspf https://raw.githubusercontent.com/AlessandroPerazzetta/vlc-media-library/main/ml.xspf
-
-cat > ~/.bash_aliases<< EOF
-alias l='ls -lah'
-alias cls='clear'
-EOF
+                printf "${LCYAN}--------------------------------------------------------------------------------\n${LRED}"
+                printf "Please download virtualbox extension pack from here: \n"
+                printf "https://www.virtualbox.org/wiki/Downloads\n"
+                printf "and install manually from VirtualBox under File > Preferences > Extensions\n"
+                printf "${LCYAN}--------------------------------------------------------------------------------\n${GREEN}"
+                read -n 1 -s -r -p "Press any key to continue"
+                printf "\n${NC}"
+                ;;
+            17)
+                printf "${YELLOW}Installing kicad...\n${NC}"
+                sudo apt-add-repository -y ppa:kicad/kicad-5.1-releases
+                sudo apt update
+                sudo apt -y install --install-recommends kicad
+                ;;
+            18)
+                printf "${YELLOW}Installing telegram...\n${NC}"
+                curl -fsSLo /tmp/Telegram.xz https://telegram.org/dl/desktop/linux
+                sudo tar -xf /tmp/Telegram.xz -C /opt/
+                ;;
+            19)
+                printf "${YELLOW}Installing rust...\n${NC}"
+                curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+                ;;
+            20)
+                printf "${YELLOW}Installing python 3.6.15 (src install)...\n${NC}"
+                sudo apt -y install build-essential checkinstall
+                sudo apt -y install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+                cd /tmp
+                sudo wget https://www.python.org/ftp/python/3.6.15/Python-3.6.15.tgz
+                sudo tar xzf Python-3.6.15.tgz
+                cd Python-3.6.15
+                sudo ./configure --enable-optimizations
+                sudo make altinstall
+                printf "${YELLOW}Installing multiple python...\n${NC}"
+                sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+                sudo update-alternatives --install /usr/bin/python3.6 python3.6 /usr/local/bin/python3.6 2
+                ;;
+        esac
+    done
+else
+        exit 0
+fi
