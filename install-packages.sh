3dced47c64f5f2aed701173e31ff4f1cb833c08c
Alessandro Perazzetta <alessandro.perazzetta@dmiweb.net>
2023-01-27 16:56:22 +0100

    Fix apt -y for spotify

--- install-packages.sh
+++ install-packages.sh
@@ -303,7 +303,7 @@ then
                 printf "${YELLOW}Installing borgbackup and vorta gui...\n${NC}"
                 curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
                 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-                sudo apt-get update && sudo apt-get install spotify-client
+                sudo apt-get update && sudo apt-get -y install spotify-client
                 curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
                 curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
                 ;;
