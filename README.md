8b6c1dc3acb1b8430cfaefcb0c040cd391cb27fa
Alessandro Perazzetta <482310+AlessandroPerazzetta@users.noreply.github.com>
2024-07-08 16:32:21 +0200

    Fix vscode extension management

--- README.md
+++ README.md
@@ -42,30 +42,34 @@ wget -O - https://raw.githubusercontent.com/AlessandroPerazzetta/mint-post-insta
 - codium
 - codium marketplace replacement (local config)
 - codium extensions installed
-  * bungcip.better-toml
-  * rust-lang.rust-analyzer
-  * jinxdash.prettier-rust
-  * kogia-sima.vscode-sailfish
-  * ms-python.python
-  * ms-python.vscode-pylance
-  * ms-vscode.cpptools
-  * serayuzgur.crates
-  * usernamehw.errorlens
-  * vadimcn.vscode-lldb
-  * jeff-hykin.better-cpp-syntax
-  * aaron-bond.better-comments
-  * vsciot-vscode.vscode-arduino
-  * kamikillerto.vscode-colorize
-  * oderwat.indent-rainbow
-  * eamodio.gitlens
-  * evgeniypeshkov.syntax-highlighter
-  * shd101wyy.markdown-preview-enhanced
+ * ["Better Comments: Improve your code commenting by annotating with alert, informational, TODOs, and more!"]="aaron-bond.better-comments"
+ * ["Better TOML: Better TOML Language support"]="bungcip.better-toml"
+ * ["Prettier - Code formatter: Code formatter using prettier"]="esbenp.prettier-vscode"
+ * ["Syntax Highlighter: Syntax highlighting based on Tree-sitter"]="evgeniypeshkov.syntax-highlighter"
+ * ["Better C++ Syntax: The bleeding edge of the C++ syntax"]="jeff-hykin.better-cpp-syntax"
+ * ["colorize: A vscode extension to help visualize css colors in files."]="kamikillerto.vscode-colorize"
+ * ["indent-rainbow: Makes indentation easier to read"]="oderwat.indent-rainbow"
+ * ["Serial Monitor: Send and receive text from serial ports."]="ms-vscode.vscode-serial-monitor"
+ * ["Arduino: Arduino for Visual Studio Code"]="vsciot-vscode.vscode-arduino"
+ * ["isort: Import organization support for Python files using isort."]="ms-python.isort"
+ * ["Pylint: Linting support for Python files using Pylint."]="ms-python.pylint"
+ * ["Python: Python language support with extension access points for IntelliSense (Pylance), Debugging (Python Debugger), linting, formatting, refactoring, unit tests, "]="ms-python.python"
+ * ["Pylance: A performant, feature-rich language server for Python in VS Code"]="ms-python.vscode-pylance"
+ * ["C/C++: C/C++ IntelliSense, debugging, and code browsing."]="ms-vscode.cpptools"
+ * ["CodeLLDB: A native debugger powered by LLDB. Debug C++, Rust and other compiled languages."]="vadimcn.vscode-lldb"
+ * ["Prettier - Code formatter (Rust): Prettier Rust is a code formatter that autocorrects bad syntax"]="jinxdash.prettier-rust"
+ * ["rust-analyzer: Rust language support for Visual Studio Code"]="rust-lang.rust-analyzer"
+ * ["crates: Helps Rust developers managing dependencies with Cargo.toml."]="serayuzgur.crates"
+ * ["Markdown Preview Enhanced: Markdown Preview Enhanced ported to vscode"]="shd101wyy.markdown-preview-enhanced"
+ * ["GitLens — Git supercharged: Supercharge Git within VS Code"]="eamodio.gitlens"
+ * ["Error Lens: Improve highlighting of errors, warnings and other language diagnostics."]="usernamehw.errorlens"
+ * ["Shades of Purple: 🦄 A professional theme suite with hand-picked & bold shades of purple for your VS Code editor and terminal apps."]="ahmadawais.shades-of-purple"
 - codium extensions uninstalled
-  * ms-toolsai.jupyter
-  * ms-toolsai.jupyter-keymap
-  * ms-toolsai.jupyter-renderers
-  * ms-toolsai.vscode-jupyter-cell-tags
-  * ms-toolsai.vscode-jupyter-slideshow
+ * ["Jupyter: Jupyter notebook support, interactive programming and computing that supports Intellisense, debugging and more."]="ms-toolsai.jupyter"
+ * ["Jupyter Keymap: Jupyter keymaps for notebooks"]="ms-toolsai.jupyter-keymap"
+ * ["Jupyter Notebook Renderers: Renderers for Jupyter Notebooks (with plotly, vega, gif, png, svg, jpeg and other such outputs)"]="ms-toolsai.jupyter-renderers"
+ * ["Jupyter Cell Tags: Jupyter Cell Tags support for VS Code"]="ms-toolsai.vscode-jupyter-cell-tags"
+ * ["Jupyter Slide Show: Jupyter Slide Show support for VS Code"]="ms-toolsai.vscode-jupyter-slideshow"
 - marktext
 - dbeaver-ce_latest_amd64
 - smartgit-23_1_2
@@ -99,44 +103,7 @@ wget -O - https://raw.githubusercontent.com/AlessandroPerazzetta/mint-post-insta
 
 > Replaced with local user .config custom product.json file
 
-# List installed codium extensions:
-
-- Better TOML Language support
-  * bungcip.better-toml
-- Rust language support for Visual Studio Code
-  * rust-lang.rust-analyzer
-- Prettier Rust is a code formatter that autocorrects bad syntax
-  * jinxdash.prettier-rust
-- Syntax Highlighting for Sailfish Templates in VSCode
-  * kogia-sima.vscode-sailfish
-- Python extension for Visual Studio Code
-  * ms-python.python
-  * ~~ms-python.vscode-pylance~~
-- C/C++ for Visual Studio Code
-  * ms-vscode.cpptools
-- Helps Rust developers managing dependencies with Cargo.toml.
-  * serayuzgur.crates
-- Improve highlighting of errors, warnings and other language diagnostics.
-  * usernamehw.errorlens
-- CodeLLDB (A native debugger powered by LLDB. Debug C++, Rust and other compiled languages)
-  * vadimcn.vscode-lldb
-- The bleeding edge of the C++ syntax
-  * jeff-hykin.better-cpp-syntax
-- Improve your code commenting by annotating with alert, informational, TODOs, and more!
-  * aaron-bond.better-comments
-- Arduino for Visual Studio Code
-  * vsciot-vscode.vscode-arduino
-- A vscode extension to help visualize css colors in files.
-  * kamikillerto.vscode-colorize
-- A simple extension to make indentation more readable
-  * oderwat.indent-rainbow
-- Git supercharged
-  * eamodio.gitlens
-- Syntax highlighting based on Tree-sitter
-  * evgeniypeshkov.syntax-highlighter
-- Markdown Preview Enhanced
-  * shd101wyy.markdown-preview-enhanced
-  
+ 
 # List selectable programming languages:
 
 - rust
--- install-packages.sh
+++ install-packages.sh
@@ -227,31 +227,54 @@ then
                     printf "${RED}Installing/Uninstalling vscodium extensions failed, codium could not be found...\n${NC}"
                 else
                     printf "${YELLOW}Installing vscodium extensions ...\n${NC}"
-                    codium --install-extension bungcip.better-toml
-                    codium --install-extension rust-lang.rust-analyzer
-                    codium --install-extension jinxdash.prettier-rust
-                    codium --install-extension kogia-sima.vscode-sailfish
-                    codium --install-extension ms-python.python
-                    codium --install-extension ms-python.vscode-pylance
-                    codium --install-extension ms-vscode.cpptools
-                    codium --install-extension serayuzgur.crates
-                    codium --install-extension usernamehw.errorlens
-                    codium --install-extension vadimcn.vscode-lldb
-                    codium --install-extension vsciot-vscode.vscode-arduino
-                    codium --install-extension jeff-hykin.better-cpp-syntax
-                    codium --install-extension aaron-bond.better-comments
-                    codium --install-extension ahmadawais.shades-of-purple
-                    codium --install-extension kamikillerto.vscode-colorize
-                    codium --install-extension oderwat.indent-rainbow
-                    codium --install-extension eamodio.gitlens
-                    codium --install-extension evgeniypeshkov.syntax-highlighter
-                    codium --install-extension shd101wyy.markdown-preview-enhanced
+                    export VSCODE_GALLERY_SERVICE_URL='https://marketplace.visualstudio.com/_apis/public/gallery'
+                    export VSCODE_GALLERY_ITEM_URL='https://marketplace.visualstudio.com/items'
+                    export VSCODE_GALLERY_CACHE_URL='https://vscode.blob.core.windows.net/gallery/index'
+                    export VSCODE_GALLERY_CONTROL_URL=''
+                    declare -A VSCODEEXTlistAdd=(
+                        ["Better Comments: Improve your code commenting by annotating with alert, informational, TODOs, and more!"]="aaron-bond.better-comments"
+                        ["Better TOML: Better TOML Language support"]="bungcip.better-toml"
+                        ["Prettier - Code formatter: Code formatter using prettier"]="esbenp.prettier-vscode"
+                        ["Syntax Highlighter: Syntax highlighting based on Tree-sitter"]="evgeniypeshkov.syntax-highlighter"
+                        ["Better C++ Syntax: The bleeding edge of the C++ syntax"]="jeff-hykin.better-cpp-syntax"
+                        ["colorize: A vscode extension to help visualize css colors in files."]="kamikillerto.vscode-colorize"
+                        ["indent-rainbow: Makes indentation easier to read"]="oderwat.indent-rainbow"
+                        ["Serial Monitor: Send and receive text from serial ports."]="ms-vscode.vscode-serial-monitor"
+                        ["Arduino: Arduino for Visual Studio Code"]="vsciot-vscode.vscode-arduino"
+                        ["isort: Import organization support for Python files using isort."]="ms-python.isort"
+                        ["Pylint: Linting support for Python files using Pylint."]="ms-python.pylint"
+                        ["Python: Python language support with extension access points for IntelliSense (Pylance), Debugging (Python Debugger), linting, formatting, refactoring, unit tests, "]="ms-python.python"
+                        ["Pylance: A performant, feature-rich language server for Python in VS Code"]="ms-python.vscode-pylance"
+                        ["C/C++: C/C++ IntelliSense, debugging, and code browsing."]="ms-vscode.cpptools"
+                        ["CodeLLDB: A native debugger powered by LLDB. Debug C++, Rust and other compiled languages."]="vadimcn.vscode-lldb"
+                        ["Prettier - Code formatter (Rust): Prettier Rust is a code formatter that autocorrects bad syntax"]="jinxdash.prettier-rust"
+                        ["rust-analyzer: Rust language support for Visual Studio Code"]="rust-lang.rust-analyzer"
+                        ["crates: Helps Rust developers managing dependencies with Cargo.toml."]="serayuzgur.crates"
+                        ["Markdown Preview Enhanced: Markdown Preview Enhanced ported to vscode"]="shd101wyy.markdown-preview-enhanced"
+                        ["GitLens — Git supercharged: Supercharge Git within VS Code"]="eamodio.gitlens"
+                        ["Error Lens: Improve highlighting of errors, warnings and other language diagnostics."]="usernamehw.errorlens"
+                        ["Shades of Purple: 🦄 A professional theme suite with hand-picked & bold shades of purple for your VS Code editor and terminal apps."]="ahmadawais.shades-of-purple"
+                    )
+                    for i in "${!VSCODEEXTlistAdd[@]}"; do
+                        #echo "Key: $i value: ${VSCODEEXTlistAdd[$i]}"
+                        printf "${LCYAN}- Extension: ${i}\n${NC}"
+                        codium --install-extension ${VSCODEEXTlistAdd[$i]} --log debug
+                        printf "\n${NC}"
+                    done
                     printf "${YELLOW}Uninstalling vscodium extensions ...\n${NC}"
-                    codium --uninstall-extension ms-toolsai.jupyter
-                    codium --uninstall-extension ms-toolsai.jupyter-keymap
-                    codium --uninstall-extension ms-toolsai.jupyter-renderers
-                    codium --uninstall-extension ms-toolsai.vscode-jupyter-cell-tags
-                    codium --uninstall-extension ms-toolsai.vscode-jupyter-slideshow
+                    declare -A VSCODEEXTlistDel=(
+                        ["Jupyter: Jupyter notebook support, interactive programming and computing that supports Intellisense, debugging and more."]="ms-toolsai.jupyter"
+                        ["Jupyter Keymap: Jupyter keymaps for notebooks"]="ms-toolsai.jupyter-keymap"
+                        ["Jupyter Notebook Renderers: Renderers for Jupyter Notebooks (with plotly, vega, gif, png, svg, jpeg and other such outputs)"]="ms-toolsai.jupyter-renderers"
+                        ["Jupyter Cell Tags: Jupyter Cell Tags support for VS Code"]="ms-toolsai.vscode-jupyter-cell-tags"
+                        ["Jupyter Slide Show: Jupyter Slide Show support for VS Code"]="ms-toolsai.vscode-jupyter-slideshow"
+                    )
+                    for i in "${!VSCODEEXTlistDel[@]}"; do
+                        #echo "Key: $i value: ${VSCODEEXTlistDel[$i]}"
+                        printf "${LCYAN}- Extension: ${i}\n${NC}"
+                        codium --uninstall-extension ${VSCODEEXTlistDel[$i]} --log debug
+                        printf "\n${NC}"
+                    done
                 fi
                 ;;
             marktext)
