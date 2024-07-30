30ce93b185b4918e0cacf55e8c56941b8f69630e
Alessandro Perazzetta <482310+AlessandroPerazzetta@users.noreply.github.com>
2024-07-30 16:23:31 +0200

    Changed echo to curl download from repo for nemo actions due to echo print command inside text of file action

--- install-packages.sh
+++ install-packages.sh
@@ -183,7 +183,8 @@ then
                 printf "${YELLOW}Installing custom Nemo Actions...\n${NC}"                
                 mkdir -p ~/.local/share/nemo/actions/
                 printf "${LCYAN}- Action: MKDTS\n${NC}"
-                bash -c "echo -e '# Custom action to create a dir with current timestamp\n[Nemo Action]\nName=MKDTS dir here\nComment=Create a dir with timestamp name\nExec=bash -c \"mkdir %F/$(date +%Y%m%d_%H%M)\"\nIcon-Name=inode-directory\nSelection=none\nExtensions=none\nDependencies=mkdir\nEscapeSpaces=true\nQuote=double' >> ~/.local/share/nemo/actions/mkdts.nemo_action"
+                #bash -c "echo -e '# Custom action to create a dir with current timestamp\n[Nemo Action]\nName=MKDTS dir here\nComment=Create a dir with timestamp name\nExec=bash -c \"mkdir %F/$(date +%Y%m%d_%H%M)\"\nIcon-Name=inode-directory\nSelection=none\nExtensions=none\nDependencies=mkdir\nEscapeSpaces=true\nQuote=double' >> ~/.local/share/nemo/actions/mkdts.nemo_action"
+                curl -fsSLo ~/.local/share/nemo/actions/mkdts.nemo_action https://raw.githubusercontent.com/AlessandroPerazzetta/mint-post-install/main/nemo_actions/mkdts.nemo_action
                 ;;
             neovim)
                 printf "${YELLOW}Installing neovim...\n${NC}"
