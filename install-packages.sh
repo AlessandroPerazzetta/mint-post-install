737f693ce0ce90695d9755cfc90ab0922c1d849a
Alessandro Perazzetta <482310+AlessandroPerazzetta@users.noreply.github.com>
2025-03-05 13:24:55 +0100

    Add solaar ppa repository for latest stable version

--- install-packages.sh
+++ install-packages.sh
@@ -614,6 +614,8 @@ then
                 ;;
             solaar)
                 printf "${YELLOW}Installing solaar (Logitech mouse support)...\n${NC}"
+                sudo add-apt-repository -y ppa:solaar-unifying/stable
+                sudo apt update
                 sudo apt-get -y install solaar
                 ;;
             borgbackup_vorta)
