ce2f03cd7ec5261c291769e999145ac5f01cfeee
Alessandro Perazzetta <482310+AlessandroPerazzetta@users.noreply.github.com>
2025-09-27 17:48:31 +0200

    Add --break-system-packages to latest pip installation step to force system installation override

--- install-packages.sh
+++ install-packages.sh
@@ -700,7 +700,7 @@ then
             latest_pip)
                 printf "${YELLOW}Installing latest pip...\n${NC}"
                 curl https://bootstrap.pypa.io/get-pip.py --output /tmp/get-pip.py
-                sudo -H python /tmp/get-pip.py 
+                sudo -H python /tmp/get-pip.py --break-system-packages
                 sudo ln -s /usr/local/bin/pip3 /usr/bin/pip3
                 ;;
             qt_stuff)
