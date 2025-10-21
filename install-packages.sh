6ca71c746ce10105a5d5428e9fa2807b91619a24
Alessandro Perazzetta <482310+AlessandroPerazzetta@users.noreply.github.com>
2025-10-22 00:04:30 +0200

    Fix zed nemo action

--- install-packages.sh
+++ install-packages.sh
@@ -694,7 +694,7 @@ then
             zed_editor_nemo_actions)
                 printf "${YELLOW}Installing nemo action for zed...\n${NC}"
                 mkdir -p ~/.local/share/nemo/actions/
-                bash -c "echo -e '[Nemo Action]\nActive=true\nName=Zed here\nComment=Launch Zed on this folder\nExec=/opt/zed-stable/libexec/zed-editor %U\nIcon-Name=zim\nSelection=none\nExtensions=any;\nQuote=double\nDependencies=zed;' > ~/.local/share/nemo/actions/zed.nemo_action"
+                bash -c "echo -e '[Nemo Action]\nActive=true\nName=Zed here\nComment=Launch Zed on this folder\nExec=/opt/zed-stable/libexec/zed-editor %U\nIcon-Name=zim\nSelection=none\nExtensions=any\nQuote=double\nDependencies=/opt/zed-stable/libexec/zed-editor\n' > ~/.local/share/nemo/actions/zed.nemo_action"
                 ;;
             marktext)
                 printf "${YELLOW}Installing Marktext editor...\n${NC}"
