0851f2859637936b84bb69249c791f43dfc9e5bc
Alessandro Perazzetta <482310+AlessandroPerazzetta@users.noreply.github.com>
2025-10-23 09:57:22 +0200

    Add VSCode MS official

--- README.md
+++ README.md
@@ -112,6 +112,49 @@ curl -s https://raw.githubusercontent.com/AlessandroPerazzetta/mint-post-install
   * REP>["Arduino: Arduino for Visual Studio Code"]="vsciot-vscode.vscode-arduino"
   * REP>["Better TOML: Better TOML Language support"]="bungcip.better-toml"
   * REM>["GitLens — Git supercharged: Supercharge Git within VS Code"]="eamodio.gitlens"
+- code
+- code nemo actions
+- code extensions installed
+  * {MARKET} ["Better Comments: Improve your code commenting by annotating with alert, informational, TODOs, and more!"]="aaron-bond.better-comments"
+  * ~~{MARKET} ["Better TOML: Better TOML Language support"]="bungcip.better-toml"~~
+  * {MARKET} ["Even Better TOML: Fully-featured TOML support"]="tamasfe.even-better-toml"
+  * {MARKET} ["Prettier - Code formatter: Code formatter using prettier"]="esbenp.prettier-vscode"
+  * {MARKET} ["Syntax Highlighter: Syntax highlighting based on Tree-sitter"]="evgeniypeshkov.syntax-highlighter"
+  * {MARKET} ["Better C++ Syntax: The bleeding edge of the C++ syntax"]="jeff-hykin.better-cpp-syntax"
+  * {MARKET} ["colorize: A vscode extension to help visualize css colors in files."]="kamikillerto.vscode-colorize"
+  * {MARKET} ["indent-rainbow: Makes indentation easier to read"]="oderwat.indent-rainbow"
+  * {MARKET} ["Serial Monitor: Send and receive text from serial ports."]="ms-vscode.vscode-serial-monitor"
+  * ~~{MARKET} ["Arduino: Arduino for Visual Studio Code"]="vsciot-vscode.vscode-arduino"~~
+  * {MARKET} ["Arduino: Arduino for Visual Studio Code Community Edition fork"]="vscode-arduino.vscode-arduino-community"
+  * {MARKET} ["isort: Import organization support for Python files using isort."]="ms-python.isort"
+  * {MARKET} ["Pylint: Linting support for Python files using Pylint."]="ms-python.pylint"
+  * {MARKET} ["Python: Python language support with extension access points for IntelliSense (Pylance), Debugging (Python Debugger), linting, formatting, refactoring, unit tests, "]="ms-python.python"
+  * {MARKET} ["Pylance: A performant, feature-rich language server for Python in VS Code"]="ms-python.vscode-pylance"
+    * {MARKET} ["CodeLLDB: A native debugger powered by LLDB. Debug C++, Rust and other compiled languages."]="vadimcn.vscode-lldb"
+  * {MARKET} ["Prettier - Code formatter (Rust): Prettier Rust is a code formatter that autocorrects bad syntax"]="jinxdash.prettier-rust"
+  * {MARKET} ["rust-analyzer: Rust language support for Visual Studio Code"]="rust-lang.rust-analyzer"
+  * {MARKET} ["Dependi: Empowers developers to efficiently manage dependencies and address vulnerabilities in Rust, Go, JavaScript, Typescript, PHP and Python projects."]="fill-labs.dependi"
+  * ~~{MARKET} ["crates: Helps Rust developers managing dependencies with Cargo.toml."]="serayuzgur.crates"~~
+  * {MARKET} ["Markdown Preview Enhanced: Markdown Preview Enhanced ported to vscode"]="shd101wyy.markdown-preview-enhanced"
+  * ~~{MARKET} ["GitLens — Git supercharged: Supercharge Git within VS Code"]="eamodio.gitlens"~~
+  * {MARKET} ["Error Lens: Improve highlighting of errors, warnings and other language diagnostics."]="usernamehw.errorlens"
+  * {MARKET} ["Todo Tree: Show TODO, FIXME, etc. comment tags in a tree view"]="Gruntfuggly.todo-tree"
+  * {MARKET} ["Shades of Purple: 🦄 A professional theme suite with hand-picked & bold shades of purple for your VS Code editor and terminal apps."]="ahmadawais.shades-of-purple"
+  * {MARKET} ["Readable Indent"]="cnojima.readable-indent"
+  * {MARKET} ["C/C++: C/C++ IntelliSense, debugging, and code browsing."]="ms-vscode.cpptools"
+  * {GITHUB} ["SSH Host Requirements"]="https://github.com/jeanp413/open-remote-ssh"
+- code extensions uninstalled
+  * ["Jupyter: Jupyter notebook support, interactive programming and computing that supports Intellisense, debugging and more."]="ms-toolsai.jupyter"
+  * ["Jupyter Keymap: Jupyter keymaps for notebooks"]="ms-toolsai.jupyter-keymap"
+  * ["Jupyter Notebook Renderers: Renderers for Jupyter Notebooks (with plotly, vega, gif, png, svg, jpeg and other such outputs)"]="ms-toolsai.jupyter-renderers"
+  * ["Jupyter Cell Tags: Jupyter Cell Tags support for VS Code"]="ms-toolsai.vscode-jupyter-cell-tags"
+  * ["Jupyter Slide Show: Jupyter Slide Show support for VS Code"]="ms-toolsai.vscode-jupyter-slideshow"
+- code extensions replaced/removed
+  * REP>["Arduino: Arduino for Visual Studio Code"]="vsciot-vscode.vscode-arduino"
+  * REP>["Better TOML: Better TOML Language support"]="bungcip.better-toml"
+  * REM>["GitLens — Git supercharged: Supercharge Git within VS Code"]="eamodio.gitlens"
+  * REM> (Integrated)["GitHub Copilot - 1.257.1316"]="GitHub.copilot"
+  * REM> (Integrated)["GitHub Copilot Chat - 0.23.2024120501"]="GitHub.copilot-chat"
 - zed editor
 - zed editor nemo actions
 - marktext
--- install-packages.sh
+++ install-packages.sh
@@ -108,9 +108,12 @@ ALL_OPTIONS=(
     "remmina|remmina|on"
     "tabby|tabby|on"
     "tabby_libgl_fix|tabby libgl fix|off"
-    "vscodium|vscodium|on"
+    "vscodium|vscodium|off"
+    "vscodium_nemo_actions|vscodium_nemo_actions|off"
+    "vscodium_ext|vscodium extensions|off"
+    "vscode|vscode|on"
     "vscode_nemo_actions|vscode_nemo_actions|on"
-    "vscodium_ext|vscodium extensions|on"
+    "vscode_ext|vscode extensions|on"
     "zed_editor|zed editor|on"
     "zed_editor_nemo_actions|zed editor nemo actions|on"
     "marktext|marktext|on"
@@ -536,7 +539,7 @@ then
                 printf "${YELLOW}Installing vscodium MS marketplace ENV in /etc/profile.d/vscode-market.sh ...\n${NC}"
                 sudo bash -c "echo -e 'export VSCODE_GALLERY_SERVICE_URL='https://marketplace.visualstudio.com/_apis/public/gallery'\nexport VSCODE_GALLERY_ITEM_URL='https://marketplace.visualstudio.com/items'\nexport VSCODE_GALLERY_CACHE_URL='https://vscode.blob.core.windows.net/gallery/index'\nexport VSCODE_GALLERY_CONTROL_URL=''' > /etc/profile.d/vscode-market.sh"
                 ;;
-            vscode_nemo_actions)
+            vscodium_nemo_actions)
                 printf "${YELLOW}Installing nemo action for vscodium...\n${NC}"
                 mkdir -p ~/.local/share/nemo/actions/
                 curl -fsSLo ~/.local/share/nemo/actions/codium.nemo_action https://raw.githubusercontent.com/AlessandroPerazzetta/nemo-actions-vscodium-launcher/main/codium.nemo_action
@@ -609,6 +612,92 @@ then
                         #echo "Key: $i value: ${VSCODEEXTlistDel[$i]}"
                         printf "${LCYAN}- Extension: ${i}\n${NC}"
                         codium --uninstall-extension ${VSCODEEXTlistDel[$i]} --log debug
+                        printf "\n${NC}"
+                    done
+                fi
+                ;;
+            vscode)
+                printf "${YELLOW}Installing vscode...\n${NC}"
+                printf "${LCYAN}--------------------------------------------------------------------------------\n${PURPLE}"
+                printf "Writing vscode repository ...\n"
+                printf "${LCYAN}--------------------------------------------------------------------------------\n${GREEN}"
+                
+                echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
+                curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+                sudo install -D -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg
+                rm -f microsoft.gpg
+                echo -e 'Types: deb\nURIs: https://packages.microsoft.com/repos/code\nSuites: stable\nComponents: main\nArchitectures: amd64,arm64,armhf\nSigned-By: /usr/share/keyrings/microsoft.gpg' | sudo tee /etc/apt/sources.list.d/vscode.sources
+                
+                sudo apt-get update && sudo apt-get -y install code
+                ;;
+            vscode_nemo_actions)
+                printf "${YELLOW}Installing nemo action for vscode...\n${NC}"
+                mkdir -p ~/.local/share/nemo/actions/
+                bash -c "echo -e '[Nemo Action]\nActive=true\nName=Code here\nComment=Launch Code on this folder\nExec=code %P\nIcon-Name=vscode\nSelection=none\nExtensions=any;\nQuote=double\nDependencies=code;' > ~/.local/share/nemo/actions/code.nemo_action"
+                ;;
+            vscodium_ext)
+                printf "${YELLOW}VSCode extensions ...\n${NC}"
+                if ! command -v code &> /dev/null
+                then
+                    printf "${RED}Installing/Uninstalling vscode extensions failed, code could not be found...\n${NC}"
+                else
+                    printf "${YELLOW}Installing vscodium extensions ...\n${NC}"
+                    
+                    # Extension removed from array:
+                    #     Temporary removed, installed from file (v1.24.5) due to errors:
+                    #     - https://github.com/VSCodium/vscodium/issues/2300
+                    #     - https://github.com/getcursor/cursor/issues/2976
+                    #    ["C/C++: C/C++ IntelliSense, debugging, and code browsing."]="ms-vscode.cpptools"
+                    printf "${LCYAN}Installing extension from file:\n${NC}"
+                    mkdir -p /tmp/vscodium_exts/ && cd /tmp/vscodium_exts/
+                    curl -s https://api.github.com/repos/jeanp413/open-remote-ssh/releases/latest | grep "browser_download_url.*vsix" | cut -d : -f 2,3 | tr -d \" | xargs curl -O -L
+                    # curl -s https://api.github.com/repos/microsoft/vscode-cpptools/releases/tags/v1.24.5 | grep "browser_download_url.*vsix"|grep "linux-x64" | cut -d : -f 2,3 | tr -d \" | xargs curl -O -L
+                    find . -type f -name "*.vsix" -exec codium --install-extension {} --force --log debug \;
+
+                    declare -A VSCODEEXTlistAdd=(
+                        ["C/C++: C/C++ IntelliSense, debugging, and code browsing."]="ms-vscode.cpptools"
+                        ["Better Comments: Improve your code commenting by annotating with alert, informational, TODOs, and more!"]="aaron-bond.better-comments"
+                        ["Even Better TOML: Fully-featured TOML support"]="tamasfe.even-better-toml"
+                        ["Prettier - Code formatter: Code formatter using prettier"]="esbenp.prettier-vscode"
+                        ["Syntax Highlighter: Syntax highlighting based on Tree-sitter"]="evgeniypeshkov.syntax-highlighter"
+                        ["Better C++ Syntax: The bleeding edge of the C++ syntax"]="jeff-hykin.better-cpp-syntax"
+                        ["colorize: A vscode extension to help visualize css colors in files."]="kamikillerto.vscode-colorize"
+                        ["indent-rainbow: Makes indentation easier to read"]="oderwat.indent-rainbow"
+                        ["Serial Monitor: Send and receive text from serial ports."]="ms-vscode.vscode-serial-monitor"
+                        ["Arduino: Arduino for Visual Studio Code Community Edition fork"]="vscode-arduino.vscode-arduino-community"
+                        ["isort: Import organization support for Python files using isort."]="ms-python.isort"
+                        ["Pylint: Linting support for Python files using Pylint."]="ms-python.pylint"
+                        ["Python: Python language support with extension access points for IntelliSense (Pylance), Debugging (Python Debugger), linting, formatting, refactoring, unit tests, "]="ms-python.python"
+                        ["Pylance: A performant, feature-rich language server for Python in VS Code"]="ms-python.vscode-pylance"
+                        ["CodeLLDB: A native debugger powered by LLDB. Debug C++, Rust and other compiled languages."]="vadimcn.vscode-lldb"
+                        ["Prettier - Code formatter (Rust): Prettier Rust is a code formatter that autocorrects bad syntax"]="jinxdash.prettier-rust"
+                        ["rust-analyzer: Rust language support for Visual Studio Code"]="rust-lang.rust-analyzer"
+                        ["Dependi: Empowers developers to efficiently manage dependencies and address vulnerabilities in Rust, Go, JavaScript, Typescript, PHP and Python projects."]="fill-labs.dependi"
+                        ["Markdown Preview Enhanced: Markdown Preview Enhanced ported to vscode"]="shd101wyy.markdown-preview-enhanced"
+                        ["Error Lens: Improve highlighting of errors, warnings and other language diagnostics."]="usernamehw.errorlens"
+                        ["Todo Tree: Show TODO, FIXME, etc. comment tags in a tree view"]="Gruntfuggly.todo-tree"
+                        ["Shades of Purple: 🦄 A professional theme suite with hand-picked & bold shades of purple for your VS Code editor and terminal apps."]="ahmadawais.shades-of-purple"
+                        ["Readable Indent"]="cnojima.readable-indent"
+                    )
+                    for i in "${!VSCODEEXTlistAdd[@]}"; do
+                        #echo "Key: $i value: ${VSCODEEXTlistAdd[$i]}"
+                        printf "${LCYAN}- Extension: ${i}\n${NC}"
+                        codium --install-extension ${VSCODEEXTlistAdd[$i]} --force --log debug
+                        printf "\n${NC}"
+                    done
+
+                    printf "${YELLOW}Uninstalling vscode extensions ...\n${NC}"
+                    declare -A VSCODEEXTlistDel=(
+                        ["Jupyter: Jupyter notebook support, interactive programming and computing that supports Intellisense, debugging and more."]="ms-toolsai.jupyter"
+                        ["Jupyter Keymap: Jupyter keymaps for notebooks"]="ms-toolsai.jupyter-keymap"
+                        ["Jupyter Notebook Renderers: Renderers for Jupyter Notebooks (with plotly, vega, gif, png, svg, jpeg and other such outputs)"]="ms-toolsai.jupyter-renderers"
+                        ["Jupyter Cell Tags: Jupyter Cell Tags support for VS Code"]="ms-toolsai.vscode-jupyter-cell-tags"
+                        ["Jupyter Slide Show: Jupyter Slide Show support for VS Code"]="ms-toolsai.vscode-jupyter-slideshow"
+                    )
+                    for i in "${!VSCODEEXTlistDel[@]}"; do
+                        #echo "Key: $i value: ${VSCODEEXTlistDel[$i]}"
+                        printf "${LCYAN}- Extension: ${i}\n${NC}"
+                        code --uninstall-extension ${VSCODEEXTlistDel[$i]} --log debug
                         printf "\n${NC}"
                     done
                 fi
