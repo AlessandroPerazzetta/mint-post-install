37d46548b9cba0716f6304f893aaa01f38ea3eca
Alessandro Perazzetta <482310+AlessandroPerazzetta@users.noreply.github.com>
2026-05-20 20:26:10 +0200

    refactor: update curl command in ferrite module for consistency

--- modules/ferrite.sh
+++ modules/ferrite.sh
@@ -6,6 +6,6 @@
 
 install_ferrite() {
     printf "${YELLOW}Installing Ferrite editor...\n${NC}"
-    curl -s https://api.github.com/repos/OlaProeis/Ferrite/releases/latest | grep -oP '"browser_download_url": "\K[^"]+\.deb' | xargs curl -LO -o /tmp/ferrite-editor_amd64.deb
+    curl -s https://api.github.com/repos/OlaProeis/Ferrite/releases/latest | grep -oP '"browser_download_url": "\K[^"]+\.deb' | xargs curl -L -o /tmp/ferrite-editor_amd64.deb
     sudo dpkg -i /tmp/ferrite-editor_amd64.deb
 }
