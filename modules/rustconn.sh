acc5af1651aa1346ec3cd32d30a90eec4e81ac91
Alessandro Perazzetta <482310+AlessandroPerazzetta@users.noreply.github.com>
2026-05-22 22:49:31 +0200

    refactor: separate update and installation commands in rustconn module

--- modules/rustconn.sh
+++ modules/rustconn.sh
@@ -15,12 +15,14 @@ install_rustconn() {
         # Enable the repository for Ubuntu 24.04 LTS (Noble)
         echo 'deb http://download.opensuse.org/repositories/home:/totoshko88:/rustconn/xUbuntu_24.04/ /'   | sudo tee /etc/apt/sources.list.d/rustconn.list
         curl -fsSL https://download.opensuse.org/repositories/home:/totoshko88:/rustconn/xUbuntu_24.04/Release.key   | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/rustconn.gpg > /dev/null
-        sudo apt update && sudo apt-get -y install rustconn
+        sudo apt update
+        sudo apt-get -y install rustconn
     elif [[ ${RELEASE_NUMBER} -eq 23 ]]; then
         # Enable the repository for Ubuntu 26.04 LTS (Resolut)
         echo 'deb http://download.opensuse.org/repositories/home:/totoshko88:/rustconn/xUbuntu_26.04/ /'  | sudo tee /etc/apt/sources.list.d/rustconn.list
         curl -fsSL https://download.opensuse.org/repositories/home:/totoshko88:/rustconn/xUbuntu_26.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/rustconn.gpg > /dev/null
-        sudo apt update && sudo apt-get -y install rustconn
+        sudo apt update
+        sudo apt-get -y install rustconn
     else
         printf "${RED}Unsupported Linux Mint version: ${RELEASE_NUMBER}. Please install rustconn manually.${NC}\n"
         return
