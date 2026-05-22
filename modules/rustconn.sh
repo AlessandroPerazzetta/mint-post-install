7740aeea25839ebbbdc54e19403bcd231d5a2fa1
Alessandro Perazzetta <482310+AlessandroPerazzetta@users.noreply.github.com>
2026-05-22 19:07:07 +0200

    refactor: add rustconn module for managing Rust installation based on Linux Mint version

---
+++ modules/rustconn.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Module: rustconn
+# DESC: Rust connection manager
+# DEFAULT: on
+# ORDER: 680
+# Called by install-packages.sh orchestrator
+
+install_rustconn() {
+    printf "${YELLOW}Installing rustconn...\n${NC}"
+    # Get current mint version
+    MINT_VERSION=$(lsb_release -rs)
+
+    # Rustconn repositories refer to Ubuntu versions, so we need to map the mint version to the corresponding ubuntu version and enable the repository
+    # - Ubuntu 25.04 LTS (Noble) <= Mint 22
+    # - Ubuntu 26.04 LTS (Resolut) >= Mint 23
+    if [[ "$MINT_VERSION" <= "22"* ]]; then
+        # Enable the repository for Ubuntu 24.04 LTS (Noble)
+        echo 'deb http://download.opensuse.org/repositories/home:/totoshko88:/rustconn/xUbuntu_24.04/ /'   | sudo tee /etc/apt/sources.list.d/rustconn.list
+        curl -fsSL https://download.opensuse.org/repositories/home:/totoshko88:/rustconn/xUbuntu_24.04/Release.key   | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/rustconn.gpg > /dev/null
+        sudo apt update && sudo apt install rustconn
+    elif [[ "$MINT_VERSION" >= "23"* ]]; then
+        # Enable the repository for Ubuntu 26.04 LTS (Resolut)
+        echo 'deb http://download.opensuse.org/repositories/home:/totoshko88:/rustconn/xUbuntu_26.04/ /'  | sudo tee /etc/apt/sources.list.d/rustconn.list
+        curl -fsSL https://download.opensuse.org/repositories/home:/totoshko88:/rustconn/xUbuntu_26.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/rustconn.gpg > /dev/null
+        sudo apt update && sudo apt install rustconn
+    else
+        printf "${RED}Unsupported Linux Mint version: $MINT_VERSION. Please install rustconn manually.${NC}\n"
+        return
+    fi
+
+}
